a46b821c4e53ba116eef2761bedd4164087bfd8d
Erza123 <kastratierza@gmail.com>
2018-01-15 19:42:00 +0000

    Forma finale

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # techstitution-er
-Practice software learning
+Techstitution final project
 
-MongoDB:
-1/Go to C://C:\Program Files\MongoDB\Server\3.6\bin
-2.type:mongod
-3.Open another CMD
-4.Go to techstitution project:
-5.type:nodemon server.js
+ 1.Ky eshte nje projekt per menaxhimin e pikave kufitare te Kosoves.
+ 2.Gjate punes per te krijuar kete projekt,jane instaluar MongoDB,node.js dhe express.js
+ 3.Eshte krijuar forma per me i fut te dhenat si dhe me zgjedh piken kufitare.
+ 4.On GitHub navigate to the main page of repository.
+ 5.Clone or download buttonUnder the repository name,click or download.
+ 6.Clone URL buttonIn the Clone with the HTTPs section,click to copy the clone URL for the repository.
+ 7.Open GitBash
+ 8.Change the current working directory to the location where you want the cloned directory to be made.
+ 9.Type git clone and then paste the URL you copied in step 5.
+ 10.git.clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY/ Press Enter.Your local clone will be created.
+ #Techstitution-er
--- server.js
+++ server.js
@@ -1,114 +1,135 @@
-const express = require('express');
-var bodyparser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser= require("body-parser");
+var path= require("path");
+var app=express();
 
-var app = express();
+// app.use("*/css",express.static('css'));
+// app.use("*/js",express.static('js'));
+
+app.use(express.static(__dirname+ '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+/*app.get('/',function(req,res){
+
+});*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//use static files
-app.use(express.static(__dirname +'/public'));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
 
 const MongoClient= require('mongodb').MongoClient;
 const mongoURL='mongodb://localhost:27017/techstitution';
-const objectId=require('mongodb').ObjectId;
+const objectId= require('mongodb').ObjectId;
 
 MongoClient.connect(mongoURL, function(err, database){
 
-    if(err){
-        console.log(err);
-    }else{
-      console.log('MongoDB Connected!');
-    }
-    qkmk=database.collection('qkmk');
-});
+   if(err){
+       console.log(err);
+   }else{
+       console.log('MongoDB Connected!');
 
-
-app.get('/', function(req, res){
-  var title='Forma e regjistrimit te pikave kufitare';
- 	res.render('index',{title:title});
-});
-
-// app.get('/show', function(req, res){
-//   var title='Lista me te dhena';
-//  	res.render('show',{title:title});
-// });
-
-app.get('/show',function(req,res){
-  var title="Lista me te dhena";
-  qkmk.find({}).toArray(function(err,docs){
-    if(err){
-      console.log(err);
-    }
-  res.render('show',{title:title, docs:docs});
-});
-});
-
-app.post('/add',function(req,res){
-  var data=req.body;
-  qkmk.insert(data,function(err,results){
-    if(err){
-      console.log(err);
-    }
-      res.redirect('/');
-  });
+   }
+   qkmk=database.collection('qkmk');
 
 });
 
 app.get('/edit/:id',function(req,res){
-  var title='Regjistrimi';
-  var id = req.params.id;
-  qkmk.findOne({_id: objectId(id)}, function(err,result){
-    if(err){
-      console.log(err);
-    }
-    res.render('edit', {title:title, doc:result});
-
-  });
-
-});
-
-app.post('/update/:id',function(req,res){
-  var data=req.body;
-  var id = objectId(req.params.id);
-  qkmk.updateOne({ _id:id}, {$set:data},
-  function(err,result){
-    if(err){
-      console.log(err);
-    }
-  res.redirect('/show');
-});
-});
-
-app.get('/delete/:id',function(req,res){
-  res.redirect('/');
-});
-
-app.get('/exercise', function(req,res){
-  var dataJson={
-    "id":1,
-    "project":"techstitution"
-
-  };
-
-  var projects=[
-    {id:1, name:"techstitution"},
-    {id:2, name:"techsperience"},
-    {id:3, name:"techforpolicy"},
-    {}
-
-  ];
-  res.render('exercise',{data:dataJson, projects:projects});
+   var title="Edito te dhenat";
+   var id=req.params.id;
+   qkmk.findOne({_id: objectId(id)},function(err,result){
+       if(err)
+       {
+           console.log(err);
+       }
+       res.render('edit',{title:title, doc:result})
+   });
 
 });
 
+
+app.post('/update/:id', function(req,res){
+   var data= req.body;
+   var id = objectId(req.params.id);
+   qkmk.updateOne({ _id:id}, {$set:data},
+       function(err,result){
+           if(err){
+               console.log(err);
+           }
+           res.redirect('/show');
+       });
+});
+
+app.get("/delete/:id",function(req,res){
+   var data=req.body;
+   var id = objectId(req.params.id);
+   qkmk.deleteOne({_id:id}, function(err,result)
+   {
+       if (err){
+           console.log(err);
+       }
+       res.redirect('/show');
+   });
+});
+
+app.get('/',function(req,res) {
+var title='Forma e regjistrimit te pikave kufitare';
+          res.render('index', {title: title});
+
+});
+
+app.get('/show',function(req,res){
+var title="Lista me te dhena";
+qkmk.find({}).toArray(function(err,docs){
+if (err)
+{
+console.log(err);
+}
+res.render('show',{title:title, docs:docs});
+
+});
+});
+
+
+
+
+app.post('/add',function(req,res){
+var data=req.body;
+qkmk.insert(data,function(err,result){
+   if(err){
+       console.log(err);
+   }
+   res.redirect('/');
+});
+});
+
+
+app.get('/exercise',function(req,res){
+   var projects=[{id:1, name:"vesa"},
+                   {id:2, name:"erza"},
+                   {id:3, name:"eriona"},
+                   ];
+
+                   var projects1=[{nr:1,emri:"vesa"},
+                                   {nr:2,emri:"diellza"},];
+                   res.render('exercise',{projects:projects},{projects1:projects1});
+});
+
+app.get('/highcharts',function(req,res){
+var title="Vizualizimet me highcharts";
+qkmk.find({}).toArray(function(err,docs){
+if (err)
+{
+console.log(err);
+}
+res.render('highcharts',{title:title, docs:docs});
+
+});
+});
 
 app.get('*',function(req,res){
-  res.send("404 not found")
+   res.send('Not Found!');
 });
 
-app.listen(3001, function()
-{
+
+app.listen(3001,function(){
   console.log('Navigate to http://localhost:3001')
 });
